638ab0601c5aa4e7914402909cdd6944a880d70a
Brendan <brendan_1391@yahoo.com>
2018-07-22 17:58:53 -0700

    Added a 42 api handler class

---
+++ server/apikeys.py
@@ -0,0 +1,4 @@
+# The uid and secret are stored in this file
+
+uid = '3c89d42ff26a6969337e414350de8436bca6a21013c1d0b926bd120661b6f5c3'
+secret = 'b53b05a2778cdc3905dbb68f6f973bc4a6fa02187d76b6c5b3945218b0896821'
