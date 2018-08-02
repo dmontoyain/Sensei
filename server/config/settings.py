0d277f08186a03c85e4a5d7db9e17cccdfc9590e
Brendan <brendan_1391@yahoo.com>
2018-08-01 19:38:57 -0700

    Clean up

--- server/config/settings.py
+++
@@ -1,6 +0,0 @@
-import config.dbConfig as Database
-import pymysql
-
-def configure(app, configSetting):
-	app.config['SQLALCHEMY_DATABASE_URI'] = Database.dbConnection(configSetting)
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
