80c41a39738e9585984316d750094d7ddb821c32
dmontoyain <dmontoyainestroza@gmail.com>
2018-08-01 19:17:39 -0700

    Moved and deleted a bunch of files

--- server/deps.sh
+++
@@ -1,5 +0,0 @@
-# api42 dependencies
-pip3 install requests
-
-# Database dependencies
-pip3 install pymysql
--- server/run.py
+++
@@ -1,6 +0,0 @@
-from app import create_app
-
-if __name__ == '__main__':
-    config_env = 'test'
-    app = create_app('test')
-    app.run(debug=True, use_reloader=False)
