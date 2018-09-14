871ac458a97871cb7d9c7a9fd39f5450c734e373
Brendan <brendan_1391@yahoo.com>
2018-09-13 21:38:04 -0700

    Appointments is now one HOC, made many small css changes

--- client/src/components/Home/Appointments.js
+++ client/src/components/Home/Appointments.js
@@ -1,5 +1,10 @@
 import React, { Component, Fragment } from 'react';
 
+// Components
+import NoData from '../Extra/NoData';
+import Avatar from '../Extra/Avatar';
+
+// API Handler
 import {
 	apiPendingAppointmentsAsMentor,
 	apiPendingAppointmentsAsUser,
@@ -7,118 +12,27 @@ import {
 	apiUserPendingAppointments,
 } from '../../apihandling/api';
 
-
-// Components
-
-import NoData from '../Extra/NoData';
-import Avatar from '../Extra/Avatar';
-
 // Security
-
 import authClient from '../../security/Authentication';
 
 // Icons
-
 import noDataOne from '../../assets/images/floatingguru.png';
 import noDataTwo from '../../assets/images/floatingguru2.png';
 
 // CSS
-
 import './Home.css'
 
-const monthsRef = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
-const daysRef = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-function formatTime(startTime) {
-	const newDate = new Date(startTime);
-	return {
-		day: daysRef[newDate.getDay()],
-		mth: monthsRef[newDate.getMonth()],
-		dayN: newDate.getDate(),
-		hr: newDate.getHours(),
-		mn: newDate.getMinutes(),
-		sc: newDate.getSeconds(),
-	}
-}
-
-// main container style
-const style = {
-	display: 'flex',
-	marginBottom: '30px',
-	borderBottom: '1px solid firebrick',
-}
-
-// Avatar styling
-const imageStyle = {
-	borderRadius: '4px',
-	maxHeight: '80px',
-	marginRight: '20px',
-}
-
-// a tag styling
-const aStyle = {
-	textDecoration: 'none',
-	color: 'darkgreen',
-}
-
-
-function formatStringForUser(obj) {
-	const { appointment, user } = obj;
-	const t = formatTime(appointment.start_time);
-	const time = new Date(appointment.start_time).toLocaleString('en-US', { weekday: 'long', month: 'short', hour: 'numeric', minute: 'numeric', hour12: true});
-	return `You have an appointment on ${time}`;
-};
-
-function formatStringForMentor(obj) {
-	const { appointment, user } = obj;
-
-	// Get formatted date
-	const t = formatTime(appointment.start_time);
-
-	const time = new Date(appointment.start_time).toLocaleString('en-US', { timeZone: 'America/Los_Angeles', weekday: 'long', month: 'short', hour: 'numeric', minute: 'numeric', hour12: true});
-	return (
-		<div key={appointment.id} style={style}>
-			<Avatar size="small" login={user.login} style={imageStyle} />
-			<div>
-				<a style={aStyle} href={`https://profile.intra.42.fr/users/${user.login}`}>
-					<h2>{`${user.login}`}</h2>
-				</a>
-				<h3>{`${time}`}</h3><br/><br/>
-			</div>
-		</div>
-	);
-};
-
-
-const Appointments = ({ ...props }) => {
-
-	const { myAppointments, noDataIcon, stringFormatter } = { ...props };
-
-	if (!myAppointments.length) {
-		return <NoData text="No Appointments" icon={noDataIcon} />;
-	}
-
-	return (
-		<div style={{ marginTop: '10px'}}>
-			{myAppointments.map(obj => stringFormatter(obj))}
-		</div>
-	);
-}
-
-// HOC that wraps the Appointments class
-const appointmentWrap = (WrappedComponent, apiCall, title, noDataIcon, stringFormatter) => {
+const appointmentWrap = (apiCall, title, noDataIcon) => {
 	class HOC extends Component {
 		constructor(props) {
 			super(props);
 			this.state = {
 				myAppointments: [],
-				noDataIcon: noDataIcon,
 			}
 		}
 
 		componentWillMount() {
-			console.log("PROFILE", authClient.profile.id);
-			apiCall.get(authClient.profile.id)
+			apiCall(authClient.profile.id)
 				.then(data => {
 					this.setState({ myAppointments: data.data === {} ? [] : data.data });
 				})
@@ -127,14 +41,46 @@ const appointmentWrap = (WrappedComponent, apiCall, title, noDataIcon, stringFor
 				});
 		}
 
+
+		formatAppointment = (obj) => {
+			// Declare variables
+			const { appointment, project, user, userMentoring } = obj;
+
+			// Seperate out the login information
+			const { login } = user ? user : userMentoring; // This is the only difference between the data returned by the apiUser... endpoint and the apiMentor... endpoint.
+
+			// Get formatted date
+			const time = new Date(appointment.start_time).toLocaleString('en-US', { timeZone: 'America/Los_Angeles', weekday: 'long', month: 'short', hour: 'numeric', minute: 'numeric', hour12: true});
+
+			// The main appointment row
+			return (
+				<div key={appointment.id} className="appointment-row-container">
+					<Avatar size="small" login={login} className="appointment-image" />
+					<div>
+						<a
+							style={{ textDecoration: 'none', color: 'darkgreen', fontSize: '1.4em' }}
+							href={`https://profile.intra.42.fr/users/${login}`}
+						>
+							{login}
+						</a>
+						<h3>
+							{time}
+						</h3>
+					</div>
+				</div>
+			);
+		}
+
 		render() {
+			const { myAppointments } = this.state;
+
+			// Set appointments to the map of divs of each appointment
+			const appointments = (myAppointments.length ? myAppointments.map(obj => this.formatAppointment(obj)) : <NoData text="No Appointments" icon={noDataIcon} />)
+
 			return (
 				<Fragment>
-					<h4 className="home-box-title">{title}</h4>
-					<WrappedComponent
-						{ ...this.state }
-						stringFormatter={stringFormatter}
-					/>
+					<h4 className="home-box-title" style={{ marginBottom: '15px' }}>{title}</h4>
+					{appointments}
 				</Fragment>
 			);
 		}
@@ -143,11 +89,9 @@ const appointmentWrap = (WrappedComponent, apiCall, title, noDataIcon, stringFor
 	return HOC;
 }
 
+const AppointmentsAsUser = appointmentWrap(apiUserPendingAppointments.get, "You will be mentored by...", noDataOne);
 
-
-const AppointmentsAsUser = appointmentWrap(Appointments, apiUserPendingAppointments, "Learning", noDataOne, formatStringForUser);
-
-const AppointmentsAsMentor = appointmentWrap(Appointments, apiMentorPendingAppointments, "Teaching", noDataTwo, formatStringForMentor);
+const AppointmentsAsMentor = appointmentWrap(apiMentorPendingAppointments.get, "You will mentor...", noDataTwo);
 
 export {
 	AppointmentsAsUser,
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -49,33 +49,20 @@ const homeBox = (WrappedComponent, className) => {
 
 // The main render of the home page
 
-class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			hello: "hello",
-		}
-	}
+const Home = ({ ...props }) => {
 
-	render() {
-		const { listOfUsers } = this.state;
-		const { className } = this.props; 
+	const Au = homeBox(AppointmentsAsUser);
+	const Am = homeBox(AppointmentsAsMentor);
 
-		const Pr = homeBox(Profile, "hb-top");
-		const Au = homeBox(AppointmentsAsUser);
-		const Am = homeBox(AppointmentsAsMentor);
-
-		return (
-			<Fragment>
-				{/* <div className="banner" /> */}
-				<div className="home-main">
-					<Pr />
-					<Au />
-					<Am />
-				</div>
-			</Fragment>
-		);
-	}
+	return (
+		<Fragment>
+			<div className="home-main">
+				<Profile />
+				<Au />
+				<Am />
+			</div>
+		</Fragment>
+	);
 }
 
 export default Home;
