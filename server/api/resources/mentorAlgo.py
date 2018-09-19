27598bd34cced557bf135f416c42372f7a851b0d
dmontoyain <dmontoyainestroza@gmail.com>
2018-09-18 21:45:34 -0700

    MentorAlgo revamped.  Error output says rating for mentorstat does not exist

--- server/api/resources/mentorAlgo.py
+++ server/api/resources/mentorAlgo.py
@@ -5,10 +5,14 @@ from api.models import Mentor
 def mentorAlgorithm(m):
 	names = []
 	weights = []
-	n = datetime.datetime.now()
+	n = datetime.datetime.now().timestamp()
 	for mentor in m:
 		stat = getattr(mentor, 'mentorstat')
-		last = mentor.last_appointment if type(mentor.last_appointment) != None else 0
+		print(stat.rating)
+		if mentor.last_appointment is not None:
+			last = mentor.last_appointment.timestamp()
+		else:
+			last = 0
 		x = float(int(mentor.finalmark) / 100) * float((n - last) / n) * float(stat.rating / 5)
 		weights.append(x)
 		names.append(mentor)
